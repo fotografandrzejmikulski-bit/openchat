3bd5d21f1cd5e7451b015a9efc9fb76daa0d317a
Andrzej M <fotografandrzejmikulski@gmail.com>
2026-09-08 00:52:36 +0200

    feat(i18n): localize authentication form

--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -21,7 +21,7 @@ export function AuthForm({
           className="font-normal text-zinc-600 dark:text-zinc-400"
           htmlFor="email"
         >
-          Email Address
+          Adres e-mail
         </Label>
 
         <Input
@@ -31,7 +31,7 @@ export function AuthForm({
           defaultValue={defaultEmail}
           id="email"
           name="email"
-          placeholder="user@acme.com"
+          placeholder="ty@przyklad.pl"
           required
           type="email"
         />
@@ -42,7 +42,7 @@ export function AuthForm({
           className="font-normal text-zinc-600 dark:text-zinc-400"
           htmlFor="password"
         >
-          Password
+          Hasło
         </Label>
 
         <Input
